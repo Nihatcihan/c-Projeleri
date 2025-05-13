d183187c4a75bc6ed00cd5322dda1298fba74946
Nihat Cihan <nihat.cihan7@gmail.com>
2025-05-13 13:49:56 +0300

    ucgen cizildi

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Üçgen yüksekliğini girin: ");
+        int yukseklik;
+
+        if (int.TryParse(Console.ReadLine(), out yukseklik) && yukseklik > 1)
+        {
+            for (int i = 0; i < yukseklik - 1; i++)
+            {
+                // Sol boşluk
+                for (int j = 0; j < yukseklik - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write("/");
+
+                // İç boşluk
+                for (int k = 0; k < i * 2; k++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine("\\"); 
+            }
+
+            // Taban çizgisi
+            Console.Write("/");
+            for (int i = 0; i < (yukseklik - 1) * 2; i++)
+            {
+                Console.Write("_");
+            }
+            Console.WriteLine("\\");
+        }
