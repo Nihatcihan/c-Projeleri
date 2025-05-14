5daeff84fc5295abb2a8af22b2309cd765e80d72
Nihat Cihan <nihat.cihan7@gmail.com>
2025-05-14 14:41:58 +0300

    tamamlandi

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Lütfen girilecek sayı adetini giriniz:");
+int sayi=Convert.ToInt32(Console.ReadLine());
+
+MutlakKareAl(sayi);
+
+
+void MutlakKareAl(int diziUzunlugu)
+{
+    List<int> liste =new List<int>();
+    int kucuk=0;
+    int buyuk=0;
+
+    for (int i = 0; i < diziUzunlugu; i++)
+    {
+        int deger=Convert.ToInt32(Console.ReadLine());
+        if(deger<67)
+            kucuk+=67-deger;
+        else
+            buyuk+=(deger-67)*(deger-67);
+    }
+    Console.Write($"{kucuk} {buyuk}");
+}
