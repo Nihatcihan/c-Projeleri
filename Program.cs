b66cfe0fdb0237fdb23867b5488ef1c1c1222be2
Nihat Cihan <nihat.cihan7@gmail.com>
2025-05-14 13:03:22 +0300

    tamamlandi

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write("Lütfen bir cümle giriniz:");
+string cumle=Console.ReadLine();
+karakterTersYazdirma(cumle);
+
+void karakterTersYazdirma(string cumle)
+{
+    string[] dizi=cumle.Split(' ');
+    string[] reverse=new string[dizi.Length];
+
+    for (int i = 0; i < dizi.Length; i++)
+    {
+        for (int j = dizi[i].Length-1; j >-1; j--)
+        {
+            reverse[i]+=dizi[i][j];
+        }
+    }
+    for (int i = 0; i < reverse.Length; i++)
+    {
+        Console.Write(reverse[i]+" ");
+    }
+}
