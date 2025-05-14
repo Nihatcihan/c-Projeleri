036b9d2d50b19318d88a9ca1fc6ea02e7fd775ab
Nihat Cihan <nihat.cihan7@gmail.com>
2025-05-14 12:43:40 +0300

    tamamlandı

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿(string x,int y)deger;
+Console.Write("Lütfen bir kelime giriniz:");
+deger.x=Console.ReadLine();
+Console.Write("Lütfen silinmesini istediğiniz index numarasını giriniz:");
+deger.y=Convert.ToInt32(Console.ReadLine());
+harfSil(deger.x,deger.y);
+
+void harfSil(string kelime, int sayi)
+{
+    List<char> chars=new List<char>();
+    if(sayi<=kelime.Length)
+    {
+        for (int i = 0; i < kelime.Length; i++)
+        {
+            
+            if(i==sayi)
+                continue;
+            chars.Add(kelime[i]);
+        }
+        for (int i = 0; i < chars.Count; i++)
+            Console.Write(chars[i]);
+    }
+    else
+        Console.WriteLine(kelime);
+
+}
