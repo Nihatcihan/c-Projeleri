919ddcb8f76c698b56503463090d01d9e7f04319
Nihat Cihan <nihat.cihan7@gmail.com>
2025-05-16 14:28:05 +0300

    tamamlandi

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("lütfen bir cümle giriniz:");
+string cumle = Console.ReadLine();
+
+KarakterDegistirme(cumle);
+
+void KarakterDegistirme(string cumle)
+{
+    string[] dizi = cumle.Split(' ');
+    string ilk;
+    string son;
+    // for (int i = 0; i < dizi.Length; i++)
+    // {
+    //     Console.Write(dizi[i] + " ");
+    // }
+    for (int i = 0; i < dizi.Length; i++)
+    {
+        ilk = Convert.ToString(dizi[i][0]);
+        son = Convert.ToString(dizi[i][dizi[i].Length - 1]);
+        if (dizi[i].Length == 1)
+        {
+            dizi[i] = dizi[i].Remove(dizi[i].Length - 1, 1);
+            dizi[i]= dizi[i].Insert(0, son);
+        }   
+        else
+        {
+            dizi[i] = dizi[i].Remove(dizi[i].Length - 1, 1);
+            dizi[i] = dizi[i].Remove(0, 1);
+            dizi[i] = dizi[i].Insert(0, son);
+            dizi[i] = dizi[i].Insert(dizi[i].Length, ilk);
+        }
+       
+     
+    }
+    for (int i = 0; i < dizi.Length; i++)
+    {
+        Console.Write(dizi[i] + " ");
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+## tek harf girişine duyarlı olarak tasarlandı
