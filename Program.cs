f69aa7720a7bb1d40b57f7046ab01f86367f62c1
Nihat Cihan <nihat.cihan7@gmail.com>
2025-05-16 14:42:09 +0300

    tamamlandi

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string sessiz = "QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm";
+
+Console.Write("Lütfen bir cümle giriniz:");
+string kontrol = Console.ReadLine();
+
+SessizHarf(kontrol, sessiz);
+
+void SessizHarf(string cumle,string sessizler)
+{
+    string[] dizi = cumle.Split(' ');
+    int x = 0;
+    for (int i = 0; i < dizi.Length; i++)
+    {
+        for (int j = 0; j < dizi[i].Length - 1; j++)
+        {
+            if (sessizler.Contains(dizi[i][j]) && sessizler.Contains(dizi[i][j + 1]))
+            {
+                Console.Write("True ");
+                x++;
+            }
+        }
+        if (x == 0)
+            Console.Write("False ");
+        else
+            x = 0;
+    }
+}
